6798f0bc5931e006fed53f88bf124d9a579e2658
Rahul Shah <rahulshah0728@gmail.com>
2024-02-26 23:55:05 +0530

    New responsive navbar component added succesfully and code pushing and old component deleted

--- src/Pages/Smartwatches.js
+++ src/Pages/Smartwatches.js
@@ -1,12 +1,11 @@
-import Navbarr from "../Components/Navbarr";
 import '../Pages/SCSS/common.scss'
 
 function smartwatches(params) {
     return(
-        // <div className="cootanier">
-        //     <>hiii this meassage is from smartwatches page</>
-        // </div>
-        <Navbarr />
+        <div className="cootanier">
+            <>hiii this meassage is from smartwatches page</>
+        </div>
+        // <Navbarr />
     )
 }
 
